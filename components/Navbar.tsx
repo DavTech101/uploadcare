582b74a4032a62aeaa3092dce70ee7c55c190b16
DavTech <dav-tech@outlook.com>
2022-02-27 23:46:20 +0100

    logo margin left configured

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import tw from 'twin.macro';
 //######### Component Styles #################
 
 const Wrapper = tw.nav`relative bg-betcity-blue font-sans py-2 px-4 flex justify-center items-center`;
-const Image = tw.img`absolute left-0 top-0 w-10 mt-2 ml-4`;
+const Image = tw.img`absolute left-0 top-0 w-10 mt-2 ml-2 md:ml-4`;
 const Logo = tw.div`py-2 text-white text-sm font-semibold md:(text-lg font-bold) `;
 
 //######### Components #################
